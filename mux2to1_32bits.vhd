10beb484fda66331762b9be403ba0fd6c67956ff
TrueMike21 <kouomoutchuenteu@gmail.com>
2026-03-18 07:38:59 -0400

    A 32 bits multiplexer is added

---
+++ mux2to1_32bits.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux2to1_32bits is
+port(
+     Sel     : in  std_logic;
+     A: in std_logic_vector(31 downto 0);
+	  B: in std_logic_vector(31 downto 0);
+	  C: out std_logic_vector(31 downto 0));
+end entity mux2to1_32bits;
+
+architecture structural of mux2to1_32bits is
+
+component mux2to1 is
+    port(
+        Sel     : in  std_logic;
+        A    : in  std_logic;
+        B     : in  std_logic;
+        C    : out std_logic
+    );
+end component;
+
+begin
+
+gen_mux : for i in 0 to 31 generate
+        u_mux : mux2to1
+            port map(
+                Sel => Sel,
+                A   => A(i),
+                B   => B(i),
+                C   => C(i)
+            );
+    end generate gen_mux;
+	 
+end architecture structural;
